eb708ab3cf0c55a75915db0f1d2530cc3de88dc5
PRPradeep25 <121714397+PRPradeep25@users.noreply.github.com>
2022-12-31 15:25:12 +0530

    Add files via upload

---
+++ Friends_names.java
@@ -0,0 +1,19 @@
+class Stars
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");	
+		System.out.println("****");	
+		System.out.println("*****");
+		System.out.println("******");	
+		System.out.println("*******");	
+		System.out.println("********");
+		System.out.println("*********");
+		System.out.println("**********");
+
+
+
+	}
+}
